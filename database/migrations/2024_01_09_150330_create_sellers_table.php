ee6d54104858d83f3500368cf7a0d18ba75f111a
Aure6 <aurelien7112001@gmail.com>
2024-01-16 15:32:19 +0100

    install breeze, pest

--- database/migrations/2024_01_09_150330_create_sellers_table.php
+++ database/migrations/2024_01_09_150330_create_sellers_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('sellers', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->unsignedBigInteger('user_id');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('company_name');
         });
     }
